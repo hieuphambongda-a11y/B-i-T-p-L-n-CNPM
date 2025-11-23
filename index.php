3170df9d29e9a0d77869b4e220b5ee11fee6890c
Phạm Trung Hiếu <hieuphambongda@gmail.com>
2025-11-23 09:37:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ if(!isset($_SESSION["user"])){
     <meta charset="UTF-8">
     <meta http-equiv= "X-UA-Compatible" content="IE=edge">
     <meta name ="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Người dùng</title>
 </head>
@@ -20,4 +19,5 @@ if(!isset($_SESSION["user"])){
         <a href = "dangxuat.php" class = "btn btn-warning">Đăng xuất</a>
     </div>
 </body>
+
 </html>
